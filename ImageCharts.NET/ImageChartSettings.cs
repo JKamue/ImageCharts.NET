42d38123b6476558f945620222c8bcedb0b1f925
JKamue <mail@jkamue.de>
2022-12-24 13:26:15 +0100

    Added settings object to hold connection information

---
+++ ImageCharts.NET/ImageChartSettings.cs
@@ -0,0 +1,40 @@
+﻿namespace ImageCharts.NET;
+
+public class ImageChartSettings
+{
+    internal int Timeout { get; }
+    private readonly string _host;
+    private readonly string _scheme;
+    private readonly int _port;
+    private readonly string _path;
+
+    public ImageChartSettings()
+    {
+        Timeout = 8000;
+        _host = "image-charts.com";
+        _scheme = "https";
+        _port = 443;
+        _path = "chart";
+    }
+
+    public ImageChartSettings(int timeout, string host, string scheme, int port, string path)
+    {
+        Timeout = timeout;
+        _host = host;
+        _scheme = scheme;
+        _port = port;
+        _path = path;
+    }
+
+    internal Uri GetUri()
+    {
+        var imageChartUriBuilder = new UriBuilder(_host)
+        {
+            Scheme = _scheme,
+            Port = _port,
+            Path = _path
+        };
+
+        return imageChartUriBuilder.Uri;
+    }
+}
